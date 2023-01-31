5872628eb9f4702a791be31ba21b3a9139304962
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-01 03:51:21 +0900

    add oop to instance;

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -12,12 +12,23 @@ class UserStorage {
     static getUsers(...fields) {
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
-            if(users.hasOwnProperty(field)) {
+            if (users.hasOwnProperty(field)) {
                 newUsers[field] = users[field];
             }
             return newUsers;
-        },{});
+        }, {});
         return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users);
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
     }
 }
 
