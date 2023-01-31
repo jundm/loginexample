07fb63a5b0a92ee007bdabaf4565c4133dec6567
devjun <devjun@devjunui-MacBook-Pro.local>
2023-01-31 19:00:22 +0900

    add control html from dom;

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
-"use strict"
-console.log('동작함');
+"use strict";
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector('button');
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id:id.value, password:password.value
+    };
+    console.log(req);
+}
