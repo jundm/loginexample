b44e80ee21277ef10a8443f2b4403a73d861bd75
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-01 20:01:30 +0900

    add register page;

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -8,6 +8,9 @@ const output = {
     },
     login: (req, res) => {
         res.render("home/login");
+    },
+    register: (req, res) => {
+        res.render("home/register");
     }
 };
 
@@ -17,6 +20,9 @@ const process = {
         const response = user.login();
         return res.json(response);
     },
+    register:(req,res)=>{
+      console.log('레지스터')
+    }
 };
 
 module.exports = {
