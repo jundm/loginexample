e22cf81b3d35eccf6b02d35d3cff49da35dad716
devjun <devjun@devjunui-MacBook-Pro.local>
2023-02-01 20:51:19 +0900

    add register validation;

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,6 +9,8 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if(!id.value) return alert("아이디를 입력해주세요.")
+    if(password !== confirmPassword) alert("비밀번호가 일치하지 않습니다.")
     const req = {
         id: id.value,
         name: name.value,
